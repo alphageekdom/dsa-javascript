0d0955731f31cd5ed2a6928208ecc49a83ed6b95
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-08 11:39:06 -0700

    Get Sum Problem

--- 01-basic-challenges-1/02-get-sum/get-sum.js
+++ 01-basic-challenges-1/02-get-sum/get-sum.js
@@ -1,3 +1,9 @@
-function getSum() {}
+function getSum(num1, num2) {
+  if (typeof num1 !== 'number' && typeof num2 !== 'number') {
+    throw new Error('Both parameters are not numbers');
+  }
+
+  return num1 + num2;
+}
 
 module.exports = getSum;
