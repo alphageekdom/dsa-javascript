23cd361a1bc7fe22a62e7afca972a52f3335d14c
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-08 12:20:10 -0700

    Find Max Number Problem

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,20 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  const newArr = [...arr];
+
+  let max = 0;
+
+  //   Loop the array
+  for (let i = 0; i < newArr.length; i++) {
+    const num = newArr[i];
+    if (num > max) {
+      max = num;
+    }
+  }
+  //   Compare prev value to the next
+  // If value > prev value replace as max
+  // return max value
+
+  return max;
+}
 
 module.exports = findMaxNumber;
