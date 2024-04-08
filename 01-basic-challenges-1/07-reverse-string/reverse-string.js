6557647377c353285f5934142012459a31141d84
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-08 13:58:08 -0700

    Reverse String Problem

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,29 @@
-function reverseString() {}
+function reverseString(str) {
+  // Copy string and place in array
+  const newArr = str.slice().split(' ');
+
+  const result = [];
+
+  //   loop array
+  for (let i = 0; i < newArr.length; i++) {
+    // split each word into array and chars in own index
+    const element = newArr[i].split('');
+    //  reverse loop through each array
+    for (let j = element.length - 1; j >= 0; j--) {
+      // add to result arr
+      result.push(element[j]);
+    }
+
+    // adds a space after each arrayed word except last
+    if (i < newArr.length - 1) {
+      result.push(' ');
+    }
+  }
+
+  //   Joins string including spaces
+  const reversed = result.join('');
+
+  return reversed;
+}
 
 module.exports = reverseString;
