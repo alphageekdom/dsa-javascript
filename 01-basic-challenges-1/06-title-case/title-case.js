214e2b78564f55c613ab12fed8008e329d8242e1
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-08 12:34:40 -0700

    Title Case Problem

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
+function titleCase(str) {
+  const newStr = str.slice().split(' ');
+
+  const strArr = [];
+
+  for (let i = 0; i < newStr.length; i++) {
+    const element = newStr[i];
+    strArr.push(element[0].toUpperCase() + element.slice(1));
+  }
+
+  //   console.log(strArr.join(' '));
+  return strArr.join(' ');
+}
 
 module.exports = titleCase;
