b79f6fc08c22cffd3705b0a5c0671c3edef6998a
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-08 11:49:09 -0700

    Calculator Problem

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,32 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (typeof num1 !== 'number' && typeof num2 !== 'number') {
+    throw new Error('Both parameters are not numbers');
+  }
+
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+
+    case '-':
+      result = num1 - num2;
+      break;
+
+    case '*':
+      result = num1 * num2;
+      break;
+
+    case '/':
+      result = num1 / num2;
+      break;
+
+    default:
+      throw new Error('Invalid operator');
+  }
+
+  return result;
+}
 
 module.exports = calculator;
