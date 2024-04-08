11c2c6a4eb8b6bd8bf6bce3ffa208160d730e6a9
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-08 12:01:26 -0700

    Count Occurences problem

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,19 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  if (typeof str !== 'string' && typeof char !== 'string') {
+    throw new Error('String and Char are not strings');
+  }
+
+  let newStr = str.slice();
+
+  let count = 0;
+
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countOccurrences;
