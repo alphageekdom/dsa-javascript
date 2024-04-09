ae2ff05339d39a10798db5783f33344ae76622cc
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-09 15:47:29 -0700

    Finding Missing Number Problem

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  const newArr = [...arr];
+
+  if (!newArr || newArr.length === 0) {
+    return undefined;
+  }
+
+  const n = newArr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+
+  let actualSum = 0;
+  for (let i = 0; i < newArr.length; i++) {
+    actualSum += newArr[i];
+  }
+
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
