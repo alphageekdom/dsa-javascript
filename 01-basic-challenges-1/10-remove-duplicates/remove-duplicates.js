13380894c4283cdbbba1c96678656274fbc14370
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-09 11:38:55 -0700

    Remove Duplicates Problem

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const newArr = [...arr];
+
+  const result = [];
+
+  for (let i = 0; i < newArr.length; i++) {
+    const char = newArr[i];
+    if (!result.includes(char)) {
+      result.push(char);
+    }
+  }
+
+  return result;
+}
 
 module.exports = removeDuplicates;
