a8188c179c356e12dd8af6f930f7b03b56d84924
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-09 16:15:31 -0700

    Find Missing Letter Problem

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,17 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  const newArr = [...arr];
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  const startIndex = alphabet.indexOf(newArr[0]);
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] !== alphabet[startIndex + i]) {
+      return alphabet[startIndex + i];
+    }
+  }
+
+  return '';
+}
 
 module.exports = findMissingLetter;
