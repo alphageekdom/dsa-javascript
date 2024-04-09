892cc67bcddf7d2998e8f80309e1175697a0a576
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-09 12:21:40 -0700

    Array Intersection Problem

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const newArr = new Set([...arr1]);
+
+  const result = [];
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (newArr.has(arr2[i])) {
+      result.push(arr2[i]);
+    }
+  }
+
+  return result;
+}
 
 module.exports = arrayIntersection;
