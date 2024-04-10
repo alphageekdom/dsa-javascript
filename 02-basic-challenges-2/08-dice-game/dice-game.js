32585933139c01f9a33c5663026970c7eb813a73
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-10 15:45:05 -0700

    Revert "Dice Game Problem"
    This reverts commit 81cb5704041f20d4a8481774a299c7fe43a063de.

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,32 +1,3 @@
-// If 7 or 11 -> win
-// If 2, 3, or 12 -> lose
-// Everything else roll again
-
-function rollDice() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-function diceGameSimulation(numSimulations) {
-  const results = [];
-
-  for (let i = 0; i < numSimulations; i++) {
-    const dice1 = rollDice();
-    const dice2 = rollDice();
-    const sum = dice1 + dice2;
-
-    let result = '';
-    if (sum === 7 || sum === 11) {
-      result = 'win';
-    } else if (sum === 2 || sum === 3 || sum === 12) {
-      result = 'lose';
-    } else {
-      result = 'roll again';
-    }
-
-    results.push({ dice1, dice2, sum, result });
-  }
-
-  return results;
-}
+function diceGameSimulation() {}
 
 module.exports = diceGameSimulation;
