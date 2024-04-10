a8f96b2aecc4b1c43fea4d36c1f007ac2a1bcb79
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-10 15:54:48 -0700

    Dice Game Problem

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -2,29 +2,29 @@ function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-// function diceGameSimulation(numSimulations) {
-//   const results = [];
+function diceGameSimulation(numSimulations) {
+  const results = [];
 
-//   for (let i = 0; i < numSimulations; i++) {
-//     const dice1 = rollDice();
-//     const dice2 = rollDice();
-//     const sum = dice1 + dice2;
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+    const sum = dice1 + dice2;
 
-//     let result = '';
+    let result = '';
 
-//     if (sum === 7 || sum === 11) {
-//       result = 'win';
-//     } else if (sum === 2 || sum === 3 || sum === 12) {
-//       result = 'lose';
-//     } else {
-//       result = 'roll again';
-//     }
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result = 'lose';
+    } else {
+      result = 'roll again';
+    }
 
-//     results.push({ dice1, dice2, sum, result });
-//   }
+    results.push({ dice1, dice2, sum, result });
+  }
 
-//   return results;
-// }
+  return results;
+}
 
 function diceGameSimulation(numSimulations) {
   const results = [];
