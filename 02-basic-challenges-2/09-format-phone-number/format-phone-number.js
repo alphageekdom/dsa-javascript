8800d4d604b8a8340a9f88688b192aa330e7eed4
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-10 16:16:47 -0700

    Format Phone Number Problem

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,10 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+  const formatted = numbers.join('');
+
+  return `(${formatted.substring(0, 3)}) ${formatted.substring(
+    3,
+    6
+  )}-${formatted.substring(6)}`;
+}
 
 module.exports = formatPhoneNumber;
