c703e778885048586c250a798c5db6ecb7e1b6bd
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-12 15:48:45 -0700

    Valid Anagrams Problem

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,25 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const countFrequency = (str) => {
+    return str.split('').reduce((acc, char) => {
+      acc[char] = (acc[char] || 0) + 1;
+      return acc;
+    }, {});
+  };
+
+  const freq1 = countFrequency(str1);
+  const freq2 = countFrequency(str2);
+
+  for (const char in freq1) {
+    if (freq1[char] !== freq2[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = validAnagrams;
