e2b7112b4dbe9c038c29b61c3a0d82f90d6c5833
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-12 20:05:33 -0700

    Analyze Car Mileage Problem

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,24 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((sum, car) => sum + car.mileage, 0);
+
+  const averageMileage = parseFloat((totalMileage / cars.length).toFixed(2));
+
+  const highestMileageCar = cars.reduce(
+    (highest, car) => (car.mileage > highest.mileage ? car : highest),
+    cars[0]
+  );
+
+  const lowestMileageCar = cars.reduce(
+    (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+    cars[0]
+  );
+
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
