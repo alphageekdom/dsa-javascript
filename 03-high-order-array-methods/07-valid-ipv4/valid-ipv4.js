3337a8cb0a37312c19b2c4288187509b34b5f88e
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-12 18:01:14 -0700

    Valid IPv4 Address Problem

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,13 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const octets = str.split('.');
+  if (octets.length !== 4) {
+    return false;
+  }
+
+  return octets.every((octet) => {
+    const number = parseInt(octet);
+    return number >= 0 && number <= 255 && String(number) === octet;
+  });
+};
 
 module.exports = isValidIPv4;
