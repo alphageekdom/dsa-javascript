0bccca9d9b9cd26ad7560ef7b4203748de1a6410
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-13 13:53:00 -0700

    Find Missing Letter Refactor Problem

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,27 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let prev = arr[0].charCodeAt(0);
+  const missingCharCode = arr
+    .map((char) => char.charCodeAt(0))
+    .find((current) => {
+      if (current - prev > 1) return true;
+      prev = current;
+    });
+
+  return missingCharCode ? String.fromCharCode(missingCharCode - 1) : '';
+}
+
+function findMissingLetter(arr) {
+  const missingCharCode = arr.filter((char, index) => {
+    if (index === 0) return false;
+    const prev = arr[index - 1].charCodeAt(0);
+    const current = char.charCodeAt(0);
+
+    return current - prev > 1;
+  })[0];
+
+  return missingCharCode
+    ? String.fromCharCode(missingCharCode.charCodeAt(0) - 1)
+    : '';
+}
 
 module.exports = findMissingLetter;
