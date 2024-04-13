b942720dfebe4e99ecd15ca298fec45c3bf630d6
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-04-12 20:42:10 -0700

    Password Validator Problem

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,19 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const validLength = password.length >= 8;
+
+  const hasUppercase = password
+    .split('')
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+  const hasLowercase = password
+    .split('')
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+  const hasDigit = password
+    .split('')
+    .some((char) => !isNaN(parseInt(char, 10)));
+
+  return validLength && hasUppercase && hasLowercase && hasDigit;
+}
 
 module.exports = validatePassword;
